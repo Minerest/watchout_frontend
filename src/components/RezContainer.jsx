7bfe84dd4056b7db85a08592c774ce5b9910392f
ericisbalanced <eric.diaz13@yahoo.com>
2019-09-22 16:34:21 -0700

    Added an origin story

--- src/components/RezContainer.jsx
+++ src/components/RezContainer.jsx
@@ -10,8 +10,7 @@ export class RezContainer extends Component {
         return(
             <div>
                 <div className="rez_header">
-                    <Header text="Hi, I'm Eric." />
-                    <Header text="This is how the site is made." />
+                    <Header text="The Watchout! Writeup" />
                 </div>
                 <Spring from={{opacity:0}} to={{opacity:1}} config={config.slow}>
                     {props =>
@@ -35,6 +34,54 @@ export class RezContainer extends Component {
                                 monthly rollup of how many crimes happen around that GPS coordinate.
                                 The laptop is an ancient Toshiba that has been sitting in the garage for years, untouched.
                                 Now, it runs my pride and joy.</p>
+                            <div>
+                                <span className="rez_header"><Header text="Origin Story" /></span>
+                                <p className="rez_paragraph">
+                                    This app began during a CSUSB Hackathon. Several teams of 5 students paired up together
+                                    to create something great. Before the Hackathon, I already had a team in mind. I went
+                                    out and recruited some of the smartest people I knew from class and convinced them
+                                    to go out on a Friday night to begin creating something.
+                                </p>
+                                <p className="rez_pagagraph">
+                                    The timeline for the Hackathon was vast. From 6pm on a Friday night all the way until
+                                    noon the next day. Wasn't the easiest thing convincing a couple of students that we
+                                    should spend our Friday night at school and spend the Saturday night recovering.
+                                    The theme for the Hackathon was "Create something that can improve the city."
+                                    That's pretty vague.
+                                </p>
+                                <p>
+                                   So we spent a couple of hours just brainstorming bad ideas and finally picked something
+                                   that we thought was pretty tight. We would create an app the monitored crime and we got
+                                   started. I started on the Python Backend. But first we needed data!
+                                </p>
+                                <p>
+                                    So I wrote a script in Visual Basic within Excel to steal data from a random Crime Watchers website.
+                                    Seemed legit. We grabbed data for the city of San Bernardino and stored it in a CSV file.
+                                    I then converted that CSV file to a JSON file with crime:coordinate pairs. Every time
+                                    a request was made with the URL parameters, I would open the file, match the closest
+                                    coordinate pair I received and sent out a small JSON tidbit of data.
+                                </p>
+                                <p>
+                                    The front end was designed with Android Studio and towards the end of the hackathon,
+                                    we had a working, functional app! All of us were crazy excited. Everything was working
+                                    flawlessly, although unoptimized. We all had to present our ideas in front of a set
+                                    of judges and when it became our turn to present, we were ready. We plugged in the HDMI
+                                    into my laptop, running an Ubuntu derivitive, and......... No output.
+                                </p>
+                                <p>
+                                    We spent our 5 minutes of fame in front of the audience trying to debug why the HDMI
+                                    wasn't outputting when one of us realized the app was no longer sending data back to the phone.
+                                    Our app CRASHED! The whole laptop was unresponsive. While trying debug what was going on,
+                                    each of us took turns on the mic talking about how the app was built, our pitfalls,
+                                    and some things that came to light while developing.
+                                </p>
+                                <p>
+                                    After the Hackathon, a couple of us continued to develop the app because we were so
+                                    pationate about the idea. I eventually created a database, a webserver, better backend
+                                    functionality, and optimized it as much I could. Towards the end of the summer, I decided
+                                    I wanted to write a front-end to this app as well so I built it with ReactJS!
+                                </p>
+                            </div>
                         </div>
                         <div id='buttons'>
                             <Button variant="contained" color="primary" size="medium" onClick={this.props.back_to_main}>HOME</Button>
