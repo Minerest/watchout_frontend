baeb6a42d90229a3d4c8b6f15641e6145f8a6d9f
ericisbalanced <eric.diaz13@yahoo.com>
2019-10-12 12:49:42 -0700

    Added SpaceX Module

---
+++ src/components/SpaceX.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import  ApolloClient  from 'apollo-boost';
+import { gql } from 'apollo-boost';
+
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+
+const GET_DATA = gql`{
+                launchesPast(limit: 10) {
+                    mission_name
+                    launch_date_local
+                    launch_site {
+                        site_name_long
+                    }
+                    links {
+                        video_link
+                        flickr_images
+                    }
+                }
+            }`
+
+export function SpaceX (props) {
+
+    const { data, loading, error } = useQuery(GET_DATA)
+    if (loading){
+        return <p>Loading</p>
+    }
+    else {
+        return (
+            <div onClick={props.back_to_main}>
+                {data.launchesPast.map(launch => (
+                    <div> {launch.mission_name} </div>
+                ))}
+            </div>
+        )
+    }
+
+    // componentDidMount() {
+    //     this.client.query({
+    //         query: gql`{
+    //             launchesPast(limit: 10) {
+    //                 mission_name
+    //                 launch_date_local
+    //                 launch_site {
+    //                     site_name_long
+    //                 }
+    //                 links {
+    //                     video_link
+    //                     flickr_images
+    //                 }
+    //             }
+    //         }`
+    //     }).then(result => {
+    //             this.setState({
+    //                 data: result.data
+    //             });
+    //             console.log(this.state.data)
+    //         }
+    //
+    //     );
+    // }
+    //
+    // render() {
+    //     if (loading) {
+    //         return (<div onClick={this.props.back_to_main} className="master_container">
+    //                     <p>Loading</p>
+    //                 </div>)
+    //     } else {
+    //         console.log(data);
+    //         return (
+    //             <div>
+    //                 {data.launchesPast[0].mission_name}
+    //             </div>
+    //         )
+    //     }
+    // }
+}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import './index.css';
 import { WatchoutContainer } from './components/watchout_container';
 import { RezContainer } from './components/RezContainer';
 import { Button} from "@material-ui/core";
-import {Header} from "./components/Header";
+import  { Header } from "./components/Header";
+import { SpaceX } from "./components/SpaceX";
+import { ApolloProvider } from "@apollo/react-hooks";
+import  ApolloClient  from 'apollo-boost';
 
 class App extends Component {
 
@@ -13,31 +16,49 @@ class App extends Component {
 		super(props);
 		this.state = {
 			watchout_showing: false,
+			spacex_showing: false,
 			that_rez_showing: false
 		}
+
+		this.client = new ApolloClient({
+			uri: "http://api.spacex.land/graphql"
+		})
+
 		this.start_the_show = this.start_the_show.bind(this);
 		this.but_that_rez_tho = this.but_that_rez_tho.bind(this);
 		this.back_to_main = this.back_to_main.bind(this);
+		this.spacex = this.spacex.bind(this);
 	}
 
 	start_the_show(){
 		this.setState({
 				watchout_showing:true,
-				that_rez_showing: false
+				that_rez_showing: false,
+				spacex_showing: false,
 		});
 	}
 
 	but_that_rez_tho(){
 		this.setState({
 			watchout_showing: false,
-			that_rez_showing: true
+			that_rez_showing: true,
+			spacex_showing: false,
 		});
+	}
+
+	spacex(){
+		this.setState({
+			watchout_showing: false,
+			that_rez_showing: false,
+			spacex_showing: true,
+		})
 	}
 
 	back_to_main(){
 		this.setState({
 			watchout_showing: false,
-			that_rez_showing: false
+			that_rez_showing: false,
+			spacex_showing: false,
 		})
 	}
 
@@ -48,6 +69,12 @@ class App extends Component {
 		else if(this.state.that_rez_showing){
 			return(<RezContainer back_to_main={this.back_to_main} start_the_show={this.start_the_show}/>);
 		}
+		else if(this.state.spacex_showing){
+			return(
+				<ApolloProvider client={this.client}>
+					<SpaceX back_to_main={this.back_to_main} />
+				</ApolloProvider>);
+		}
 		else{
 			return (
 				<div className="master_container">
@@ -55,6 +82,7 @@ class App extends Component {
 					<div className="main_buttons">
 						<span className="home_button"><Button variant="contained" color="primary" size="medium" onClick={this.start_the_show}>The Watchout! App</Button></span>
 						<Button variant="contained" color="primary" size="medium" onClick={this.but_that_rez_tho}>But how is this made?</Button>
+						<Button variant="contained" color="primary" size="medium" onClick={this.spacex}>Random Space X Stuff</Button>
 					</div>
 				</div>
 			)
