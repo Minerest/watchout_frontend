8e078f761fd1c3f1c02736fbd32d27171c374ee9
ericisbalanced <eric.diaz13@yahoo.com>
2019-10-12 14:48:13 -0700

    Added a HOME button on spacex

--- src/components/SpaceX.jsx
+++ src/components/SpaceX.jsx
@@ -3,6 +3,7 @@ import  ApolloClient  from 'apollo-boost';
 import { gql } from 'apollo-boost';
 import {SpaceXCard} from './SpaceXCard';
 import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import { Button} from "@material-ui/core";
 
 const GET_DATA = gql`{
                 launchesPast(limit: 10) {
@@ -22,8 +23,11 @@ export function SpaceX (props) {
         return <p>Loading</p>
     } else {
         return (
-            <div  className="spacex_container">
-                { data.launchesPast.map( launch => <SpaceXCard launch={ launch } key={ launch.mission_name }/> ) }
+            <div>
+                <Button variant="contained" color="primary" size="medium" onClick={props.back_to_main}>HOME</Button>
+                <div  className="spacex_container">
+                    { data.launchesPast.map( launch => <SpaceXCard launch={ launch } key={ launch.mission_name }/> ) }
+                </div>
             </div>
         )
     }
